80bd60e2401166e9671691cab58a67d7b1639828
unknown <andre.defremont@equilibriumweb.com>
2020-02-18 15:06:52 -0300

    first comit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,113 @@
+<template>
+<v-card
+    class="mx-auto my-10"
+    max-width="450"
+  >
+  
+    <v-toolbar
+    >
+      <v-text-field
+        hide-details
+        single-line
+      ></v-text-field>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+    </v-toolbar>
+     <v-stepper v-model="e6" vertical>
+    <v-stepper-step :complete="e6 > 1" step="1" editable>
+      Select an app
+      <small>Summarize if needed</small>
+    </v-stepper-step>
+
+    <v-stepper-content step="1">
+      <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+      <v-btn color="primary" @click="e6 = 2">Continue</v-btn>
+      <v-btn text>Cancel</v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step :complete="e6 > 2" step="2" editable>Configure analytics for this app</v-stepper-step>
+
+    <v-stepper-content step="2">
+      <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+      <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
+      <v-btn text>Cancel</v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step :complete="e6 > 3" step="3">Select an ad format and name ad unit</v-stepper-step>
+
+    <v-stepper-content step="3">
+      <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+      <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
+      <v-btn text>Cancel</v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step step="4">View setup instructions</v-stepper-step>
+    <v-stepper-content step="4">
+      <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+      <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
+      <v-btn text>Cancel</v-btn>
+    </v-stepper-content>
+  </v-stepper>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data: () => ({
+        e6: 1,
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/layout/pre-defined',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>
